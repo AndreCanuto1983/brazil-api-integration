274eba0848eb786802159027b5f4fabcc5da568f
André <andrecanuto@outlook.com.br>
2022-10-24 11:03:50 -0300

    feat: Adding color policies

--- Brazil.Api.Integration/Program.cs
+++ Brazil.Api.Integration/Program.cs
@@ -10,18 +10,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
+//set especific cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins(
+            "http://localhost:8080",
+            "http://192.168.0.195:8080",
+            "http://localhost:3000"));
+});
+
 var app = builder.Build();
 
 app.MapHealthChecks("/healthcheck");
 app.CustomExceptionMiddleware();
 
-//habilitar caso queira esconder o swagger após publicado em produção
+//Enable if you want to hide the swagger after publishing in production
 //if (app.Environment.IsDevelopment())
 //{
 //    app.UseSwagger();
 //    app.UseSwaggerUI();
 //}
 
+app.UseCors("AllowSpecificOrigin");
 app.UseSwagger();
 app.UseSwaggerUI();
 
