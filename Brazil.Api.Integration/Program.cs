6c890f6281d235899379cfdebfff3dbe89850f63
André <andrecanuto@outlook.com.br>
2022-11-14 12:56:24 -0300

    Melhorias e aplicação do https

--- Brazil.Api.Integration/Program.cs
+++ Brazil.Api.Integration/Program.cs
@@ -11,24 +11,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors();
+var allowSpecificOrigins = "MyPolicy";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(allowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "http://192.168.0.195:8080",
+                "http://192.168.0.197:8080",
+                "https://pos-puc-front.web.app"
+                )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
 app.MapHealthChecks("/healthcheck");
 app.CustomExceptionMiddleware();
-
-app.UseCors(x => x
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            );
-
+app.UseCors(allowSpecificOrigins);
 app.UseSwagger();
 app.UseSwaggerUI();
-
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
